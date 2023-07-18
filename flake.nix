bb527dcdfe9306c99e0cc3db4b82d853138029a7
Justin Bailey <justin.bailey@well.co>
2023-07-18 09:51:27 -0700

    Update to nixpkgs haskell tools.

--- flake.nix
+++ flake.nix
@@ -1,43 +1,67 @@
 {
   description = "Replacement for the double conversion project";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs-path.url = "nixpkgs/release-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs-path, flake-utils, haskellNix }: 
+  outputs = { self, nixpkgs-path, flake-utils }: 
     let
       inherit (flake-utils.lib.system) 
         x86_64-linux 
         x86_64-darwin 
         aarch64-darwin;
     in flake-utils.lib.eachSystem [ x86_64-linux x86_64-darwin aarch64-darwin ] (system: 
-      let
-        overlays = [ haskellNix.overlay
-          (final: prev: {
-            # This overlay adds our project to pkgs
-            double-conversion-hs =
-              final.haskell-nix.project' {
-                src = ./.;
-                compiler-nix-name = "ghc945";
-                # This is used by `nix develop .` to open a shell for use with
-                # `cabal`, `hlint` and `haskell-language-server`
-                shell.tools = {
-                  cabal = { };
-                  ghcid = { };
-                };
-                # Non-Haskell shell tools go here
-                shell.buildInputs = with pkgs; [
-                  nixpkgs-fmt
-                  prev.zlib.dev
-                ];
-              };
-          })
-        ];
-        pkgs = import nixpkgs-path { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.double-conversion-hs.flake {
+    let
+      nixpkgs = import nixpkgs-path { inherit system; };
+      inherit (nixpkgs) callPackage;
+      buildSettings =
+        _: hprev: {
+          mkDerivation = args: hprev.mkDerivation ({
+            doCheck = false;
+            doBenchmark = false;
+            doHoogle = false;
+            doHaddock = false;
+            enableLibraryProfiling = false;
+            enableExecutableProfiling = false;
+          } // args);
         };
-    in flake // {
-        # Built by `nix build .`
-        packages.default = flake.packages."double-conversion-hs:lib:double-conversion-hs";
+      shellBuildSettings = (old: {
+        overrides = nixpkgs.lib.composeExtensions (old.overrides or (_: _: { })) (_: hprev: {
+            mkDerivation = args:
+              hprev.mkDerivation ({
+                doCheck = false;
+                doBenchmark = false;
+                doHoogle = true;
+                doHaddock = true;
+                enableLibraryProfiling = false;
+                enableExecutableProfiling = false;
+              } // args);
+          });
+      });
+      # empty for now
+      haskellPkgSetOverlay = nixpkgs.callPackage (_: (_:_: {})) { };
+      double-conversion-overlay = hfinal: hprev: 
+        let
+          inherit (hfinal) callCabal2nix; 
+        in { double-conversion-hs = callCabal2nix "double-conversion-hs" ./. { }; };
+      haskell-pkgs = nixpkgs.haskell.packages.ghc944.override (
+          old: {
+            overrides = builtins.foldl' nixpkgs.lib.composeExtensions (old.overrides or (_: _: { })) [buildSettings haskellPkgSetOverlay double-conversion-overlay];
+          }
+        );
+      double-conversion-hs = haskell-pkgs.double-conversion-hs;
+    in {
+        packages.default = double-conversion-hs;
+        packages.double-conversion-hs = double-conversion-hs;
+        devShells.default = (haskell-pkgs.override shellBuildSettings).shellFor {
+          buildInputs = builtins.attrValues (
+            { 
+              inherit(haskell-pkgs)
+                cabal-install
+                ghcid;
+            }
+          );
+
+          packages = p: [p.double-conversion-hs];
+        };
     });
 
   nixConfig = {
@@ -45,7 +69,7 @@
     extra-trusted-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
     allow-import-from-derivation = "true";
-    bash-prompt-prefix = "double-conversion-hs >";
+    bash-prompt-prefix = "double-conversion-hs ";
   };
 
 }
